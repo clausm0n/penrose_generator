9083bfd5879171c325b0e42efc8f0008e05e077f
Isaac Clausman <devslave@outlook.com>
2024-05-15 12:32:36 -0400

    updated color randomizer

--- penrose_generator.py
+++ penrose_generator.py
@@ -42,13 +42,6 @@ def setup_projection(width, height):
 def update_toggles(shaders):
     global config_data, running
     print("Updating toggles...", update_event.is_set(), toggle_shader_event.is_set(), toggle_regions_event.is_set(), toggle_gui_event.is_set(), randomize_colors_event.is_set())
-    if update_event.is_set():
-        config_data = op.read_config_file(CONFIG_PATH)
-        print("config_data updated...")
-        update_event.clear()
-    if toggle_shader_event.is_set():
-        shaders.next_shader()
-        toggle_shader_event.clear()
     if randomize_colors_event.is_set():
         for i in range(3):
             config_data['color1'][i] = np.random.randint(0, 256)
@@ -56,6 +49,13 @@ def update_toggles(shaders):
         op.update_config_file(CONFIG_PATH, **config_data)
         print("Randomizing colors...")
         randomize_colors_event.clear()
+    if update_event.is_set():
+        config_data = op.read_config_file(CONFIG_PATH)
+        print("config_data updated...")
+        update_event.clear()
+    if toggle_shader_event.is_set():
+        shaders.next_shader()
+        toggle_shader_event.clear()
     if shutdown_event.is_set():
         running = False
         print("Exiting...")
