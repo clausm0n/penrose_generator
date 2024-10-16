6a63ac0c7c65afbee1fc0b2b8a9647b02d27f41c
Isaac Clausman <devslave@outlook.com>
2024-10-16 15:44:53 -0400

    pre flight

--- penrose_generator.py
+++ penrose_generator.py
@@ -13,8 +13,8 @@ import signal
 # Configuration and initialization
 CONFIG_PATH = 'config.ini'
 DEFAULT_CONFIG = {
-    'size': 23,
-    'scale': 16,
+    'size': 15,
+    'scale': 15,
     'gamma': [1.0, 0.7, 0.5, 0.3, 0.1],
     'color1': [205, 255, 255],
     'color2': [0, 0, 255]
