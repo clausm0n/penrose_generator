bac3efad0859fc35718639222eb945da354b2e67
Isaac Clausman <isaacclausman@gmail.com>
2024-11-10 21:13:22 -0500

    added multithreading

--- penrose_tools/Shaders.py
+++ penrose_tools/Shaders.py
@@ -26,7 +26,7 @@ class Shader:
 
     def next_shader(self):
         self.shader_index = (self.shader_index + 1) % len(self.shaders)
-        logging.info(f"Switched to shader index: {self.current_shader_index}")
+        logging.info(f"Switched to shader index: {self.shader_index}")
         return self.shader_index
     
     def current_shader(self):
