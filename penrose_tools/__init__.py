1f1aeb409103156b8ecf460c205364645367e8f4
Isaac Clausman <isaacclausman@gmail.com>
2024-11-10 21:10:28 -0500

    added multithreading

--- penrose_tools/__init__.py
+++ penrose_tools/__init__.py
@@ -1,5 +1,5 @@
 from .Operations import Operations
-from .Server import run_server, update_event, toggle_shader_event, toggle_regions_event, toggle_gui_event, shutdown_event, randomize_colors_event
+from .Server import run_server
 from .Shaders import Shader
 from .Tile import Tile
 # from .BluetoothServer import BluetoothServer
