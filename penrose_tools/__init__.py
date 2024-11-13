02f3524bd473ca0bca0da70a36e0e7b687e2d8fb
Isaac Clausman <isaacclausman@gmail.com>
2024-11-13 16:21:14 -0500

    updated to better udids for services

--- penrose_tools/__init__.py
+++ penrose_tools/__init__.py
@@ -5,5 +5,5 @@ from .Tile import Tile
 from .PenroseBluetoothServer import PenroseBluetoothServer
 from .PenroseBluetoothServer import run_bluetooth_server
 from .events import update_event, toggle_shader_event, randomize_colors_event, shutdown_event, toggle_regions_event, toggle_gui_event
-from .OptimizedRenderer import OptimizedRenderer
-from .ShaderManager import ShaderManager
+# from .OptimizedRenderer import OptimizedRenderer
+# from .ShaderManager import ShaderManager
