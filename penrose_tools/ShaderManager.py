79cb18171363f4e4d64288f0bceaa2f4a750fdae
Isaac Clausman <isaacclausman@gmail.com>
2024-11-13 15:34:36 -0500

    updated to better udids for services

---
+++ penrose_tools/ShaderManager.py
@@ -0,0 +1,65 @@
+#penrose_tools/ShaderManager.py
+import os
+from OpenGL.GL import *
+from OpenGL.GL import shaders
+import logging
+
+class ShaderManager:
+    def __init__(self, shaders_folder='Shaders'):
+        self.shaders_folder = shaders_folder
+        self.shader_programs = []
+        self.shader_names = []
+        self.current_shader_index = 0
+        self.logger = logging.getLogger('ShaderManager')
+        self.load_shaders()
+
+    def load_shaders(self):
+        # List of shader file pairs
+        shader_files = [
+            ('no_effect.vert', 'no_effect.frag'),
+            ('shift_effect.vert', 'shift_effect.frag'),
+            ('raindrop_ripple.vert', 'raindrop_ripple.frag'),
+            ('color_wave.vert', 'color_wave.frag'),
+            ('region_blend.vert', 'region_blend.frag'),
+            ('pixelation_slideshow.vert', 'pixelation_slideshow.frag'),
+            # Add other shader file pairs here
+        ]
+
+        for vert_file, frag_file in shader_files:
+            shader_name = vert_file.replace('.vert', '')
+            vert_path = os.path.join(self.shaders_folder, vert_file)
+            frag_path = os.path.join(self.shaders_folder, frag_file)
+            try:
+                program = self.compile_shader_program(vert_path, frag_path)
+                self.shader_programs.append(program)
+                self.shader_names.append(shader_name)
+                self.logger.info(f"Loaded shader: {shader_name}")
+            except Exception as e:
+                self.logger.error(f"Error loading shader {shader_name}: {e}")
+
+    def compile_shader_program(self, vertex_path, fragment_path):
+        with open(vertex_path, 'r') as f:
+            vertex_src = f.read()
+        with open(fragment_path, 'r') as f:
+            fragment_src = f.read()
+
+        try:
+            vertex_shader = shaders.compileShader(vertex_src, GL_VERTEX_SHADER)
+            fragment_shader = shaders.compileShader(fragment_src, GL_FRAGMENT_SHADER)
+            program = shaders.compileProgram(vertex_shader, fragment_shader)
+            return program
+        except Exception as e:
+            self.logger.error(f"Shader compilation error: {e}")
+            raise
+
+    def next_shader(self):
+        self.current_shader_index = (self.current_shader_index + 1) % len(self.shader_programs)
+        self.logger.info(f"Switched to shader index: {self.current_shader_index}")
+        return self.current_shader_index
+
+    def current_shader_program(self):
+        return self.shader_programs[self.current_shader_index]
+
+    def reset_state(self):
+        # Implement if needed
+        pass
