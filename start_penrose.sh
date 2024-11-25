00947c8067a2377f55f0c1f7f0586c8bbd0263c1
Isaac Clausman <isaacclausman@gmail.com>
2024-11-25 15:05:41 -0500

    fixed service

--- start_penrose.sh
+++ start_penrose.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 LOGFILE=/home/modus/startup.log
-VENV_PATH=/home/modus/penrose_generator/venv
+VENV_PATH=/home/modus/penrose_generator/.venv
 
 log_message() {
     echo "$(date): $1" >> $LOGFILE 2>&1
@@ -13,25 +13,8 @@ export DISPLAY=:0
 export XAUTHORITY=/home/modus/.Xauthority
 export XDG_RUNTIME_DIR=/run/user/$(id -u)
 
-env >> $LOGFILE 2>&1
-
-counter=0
-while ! xset q &>/dev/null; do
-    sleep 1
-    counter=$((counter + 1))
-    log_message "Waited $counter seconds for X server"
-    if [ $counter -gt 60 ]; then
-        log_message "X server did not start within 60 seconds, exiting"
-        exit 1
-    fi
-done
-
-log_message "Activating virtual environment"
-source $VENV_PATH/bin/activate
-
-log_message "Running penrose generator"
 cd /home/modus/penrose_generator
-python penrose_generator.py --fullscreen -bt >> $LOGFILE 2>&1
+$VENV_PATH/bin/python penrose_generator.py --fullscreen -bt >> $LOGFILE 2>&1
 EXEC_STATUS=$?
 
 log_message "Generator exited with status $EXEC_STATUS"
